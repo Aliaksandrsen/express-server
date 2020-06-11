334760730f33c8c6f5776e71f80e0282520b1108
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-06-11 23:42:13 +0300

    feat: add new route

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const products = ['Apple', 'Pen', 'Computer'];
+
 app.get('/', (req, res, next) => {
     res.send('It is working');
 });
 
+app.get('/products', (req, res, next) => {
+    res.send(products);
+});
 
 app.listen(5000, () => {
     console.log('it is started', new Date());
